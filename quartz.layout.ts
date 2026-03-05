e50653e8376314b955288aab1dfa27e13180860e
process-cxr <fantastique0910@gmail.com>
2026-03-05 15:57:51 +0800

    update

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({ localGraph: { collapseByDefault: false } }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
